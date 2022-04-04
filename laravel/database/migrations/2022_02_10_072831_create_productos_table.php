96b0dce33b4de3b7669795dab904e65386408b36
Jose-FD <jf153413@gmail.com>
2022-04-04 16:44:31 -0500

    Subiendo API

---
+++ laravel/database/migrations/2022_02_10_072831_create_productos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('productos', function (Blueprint $table) {
+            $table->uuid("id")->primary();
+            $table->string("codigo",20);
+            $table->string("nombre",100);
+            $table->string("descripcion",100);
+            $table->string("status",10);
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('productos');
+    }
+};
